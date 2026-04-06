9810f705e6b83020d3505a97ee55815ad1c35677
nailahmarsa <nailahmarsaatmojo@gmail.com>
2026-04-06 10:08:00 +0700

    login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { DM_Sans } from 'next/font/google'
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700']
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
